37170fac0cd1d4955f36ce5c74ea7fbb985bf9e2
AnNguyen <nguyenlebinhank63@gmail.com>
2025-06-10 12:42:59 +0700

    First Commit

---
+++ src/main/java/com/mycompany/pro_assignment/PRO_Assignment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pro_assignment;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class PRO_Assignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
