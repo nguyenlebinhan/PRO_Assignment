cc3d96c9dce3d3e4c18264c1ba37e37636184665
An <nguyenlebinhank63@gmail.com>
2025-06-14 19:27:31 +0700

    Update branchList

--- src/main/java/CarPrj/main/CarManager.java
+++ src/main/java/CarPrj/main/CarManager.java
@@ -2,15 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package CarPrj;
+package CarPrj.main;
 
 /**
  *
  * @author ADMIN
  */
+import CarPrj.lists.BrandList;
+
 public class CarManager {
+    
+    public static final String FILE_PATH = "C:\\Users\\ADMIN\\Desktop\\PRO_Assignment\\src\\main\\java\\CarPrj\\data\\brands.txt";
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        BrandList brandList=new BrandList();
+        
+        brandList.loadFromFile(FILE_PATH);
+        brandList.listBranch();
+        System.out.println(brandList.searchID("B5-X20"));
     }
 }
